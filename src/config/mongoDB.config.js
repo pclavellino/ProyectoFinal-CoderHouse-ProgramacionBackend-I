df52a67eed148fce645ec608276fdc96854f28ee
Pablo Clavellino <pclavellino@hotmail.com>
2024-06-18 19:56:15 -0300

    Correccion de errores

--- src/config/mongoDB.config.js
+++ src/config/mongoDB.config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectMongoDB = async () => {
     try {
-        mongoose.connect("mongodb+srv://pclavellino:Mongo2024@coderbackend-i.urz19td.mongodb.net/ProyectoFinal-Backend-1")
+        mongoose.connect("")
         console.log("Mongo DB Conectado")
     } catch(error) {
         console.log(`${error}`)
