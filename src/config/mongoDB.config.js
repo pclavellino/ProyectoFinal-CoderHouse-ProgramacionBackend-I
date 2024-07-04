542dafb9aa1af83e2f60606090af1c97bbbaa9e5
Pablo Clavellino <pclavellino@hotmail.com>
2024-07-04 19:05:11 -0300

    Version final

--- src/config/mongoDB.config.js
+++ src/config/mongoDB.config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectMongoDB = async () => {
     try {
-        mongoose.connect("")
+        mongoose.connect("mongodb+srv://pclavellino:Mongo2024@coderbackend-i.urz19td.mongodb.net/ProyectoFinal-Backend-1")
         console.log("Mongo DB Conectado")
     } catch(error) {
         console.log(`${error}`)
